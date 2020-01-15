c67e3f954c934ca0804eea6cd4c43ff595372354
Brij Mohan Lal Srivastava <brij.srivastava@inria.fr>
2020-01-15 16:58:59 +0100

    training scripts for AM and NSF

--- baseline/run.sh
+++ baseline/run.sh
@@ -51,12 +51,12 @@ anon_data_suffix=_anon_${pseudo_xvec_rand_level}_${cross_gender}_${distance}
 
 # Download pretrained models
 if [ $stage -le -1 ]; then
-  printf "${GREEN}\nDownloading all the pretrained models.${NC}\n"
+  printf "${GREEN}\nStage -1: Downloading all the pretrained models.${NC}\n"
   local/download_pretrained.sh
 fi
 
 if [ $stage -le 0 ] && false; then
-  printf "${GREEN}\nPreparing training data for AM and NSF models.${NC}\n"
+  printf "${GREEN}\nStage 0: Preparing training data for AM and NSF models.${NC}\n"
   local/data_prep_libritts.sh ${libritts_corpus}/train-clean-100 data/${am_nsf_train_data}
   
   local/run_prepfeats_am_nsf.sh --nj $nj \
@@ -69,27 +69,27 @@ if [ $stage -le 0 ] && false; then
 fi
 
 if [ $stage -le 1 ] && false; then
-  printf "${GREEN}\nTraining AM model.${NC}\n"
+  printf "${GREEN}\nStage 1: Training AM model.${NC}\n"
   local/vc/am/00_run.sh ${data_netcdf}
 fi
 
 if [ $stage -le 2 ] && false; then
-  printf "${GREEN}\nTraining NSF model.${NC}\n"
+  printf "${GREEN}\nStage 2: Training NSF model.${NC}\n"
   local/vc/nsf/00_run.sh ${data_netcdf}
 fi
 
 # Extract xvectors from anonymization pool
-if [ $stage -le 0 ]; then
+if [ $stage -le 3 ]; then
   # Prepare data for libritts-train-other-500
   local/data_prep_libritts.sh ${libritts_corpus}/train-other-500 data/${anoni_pool}
-  printf "${GREEN}\nStage 0: Extracting xvectors for anonymization pool.${NC}\n"
+  printf "${GREEN}\nStage 3: Extracting xvectors for anonymization pool.${NC}\n"
   local/featex/01_extract_xvectors.sh --nj $nj data/${anoni_pool} ${xvec_nnet_dir} \
 	  ${anon_xvec_out_dir}
 fi
 
 # Make evaluation data
-if [ $stage -le 1 ]; then
-  printf "${GREEN}\nStage 1: Making evaluation data${NC}\n"
+if [ $stage -le 4 ]; then
+  printf "${GREEN}\nStage 4: Making evaluation data${NC}\n"
   python local/make_librispeech_eval2.py proto/eval2 ${librispeech_corpus} "" || exit 1;
 
   # Sort and fix all data directories
@@ -108,8 +108,8 @@ if [ $stage -le 1 ]; then
 fi
 
 # Extract xvectors from data which has to be anonymized
-if [ $stage -le 2 ]; then
-  printf "${GREEN}\nStage 2: Anonymizing eval2 data.${NC}\n"
+if [ $stage -le 5 ]; then
+  printf "${GREEN}\nStage 5: Anonymizing eval2 data.${NC}\n"
   for name in $eval2_enroll $eval2_trial; do
     local/anon/anonymize_data_dir.sh --nj $nj --anoni-pool ${anoni_pool} \
 	 --data-netcdf ${data_netcdf} --ivec-extractor ${ivec_extractor} \
@@ -123,8 +123,8 @@ if [ $stage -le 2 ]; then
   done
 fi
 
-if [ $stage -le 3 ]; then
-  printf "${GREEN}\nStage 3: Evaluate the dataset using speaker verification.${NC}\n"
+if [ $stage -le 6 ]; then
+  printf "${GREEN}\nStage 6: Evaluate the dataset using speaker verification.${NC}\n"
   printf "${RED}**Exp 0.2 baseline: Eval 2, enroll - original, trial - original**${NC}\n"
   local/asv_eval.sh ${eval2_enroll} ${eval2_trial} || exit 1;
   printf "${RED}**Exp 3: Eval 2, enroll - original, trial - anonymized**${NC}\n"
@@ -134,8 +134,8 @@ if [ $stage -le 3 ]; then
 fi
 
 # Not anonymizing train-clean-360 here since it takes enormous amount of time and memory
-if [ $stage -le 4 ] && false; then
-  printf "${GREEN}\nStage 6: Anonymizing train data for Informed xvector model.${NC}\n"
+if [ $stage -le 7 ] && false; then
+  printf "${GREEN}\nStage 7: Anonymizing train data for Informed xvector model.${NC}\n"
   local/data_prep_adv.sh ${librispeech_corpus}/train-clean-360 data/train_clean_360
   
   local/anon/anonymize_data_dir.sh --nj $nj --stage 4 --anoni-pool ${anoni_pool} \
@@ -151,8 +151,8 @@ if [ $stage -le 4 ] && false; then
   axvec_train_data=train_clean_360${anon_data_suffix}
 fi
 
-if [ $stage -le 5 ]; then
-  printf "${GREEN}\nStage 7: Anonymizing dev-clean data for intelligibility assessment.${NC}\n"
+if [ $stage -le 8 ]; then
+  printf "${GREEN}\nStage 8: Anonymizing dev-clean data for intelligibility assessment.${NC}\n"
   local/data_prep_adv.sh ${librispeech_corpus}/dev-clean data/dev_clean
   
   local/anon/anonymize_data_dir.sh --nj $nj --anoni-pool ${anoni_pool} \
@@ -166,8 +166,8 @@ if [ $stage -le 5 ]; then
 	 dev_clean || exit 1;
 fi
 
-if [ $stage -le 6 ]; then
+if [ $stage -le 9 ]; then
   asr_eval_data=dev_clean${anon_data_suffix}
-  printf "${GREEN}\nStage 8: Performing intelligibility assessment using ASR decoding on ${asr_eval_data}.${NC}\n"
+  printf "${GREEN}\nStage 9: Performing intelligibility assessment using ASR decoding on ${asr_eval_data}.${NC}\n"
   local/asr_eval.sh --nj $nj ${asr_eval_data} exp/asr_eval_model
 fi
