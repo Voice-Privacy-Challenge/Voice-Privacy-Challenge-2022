628d3bcd7be9e6503c9916b2f7c0a465f252156a
N.T <tomashenko.n@gmail.com>
2020-01-20 01:05:59 +0100

    remove env.sh

--- env.sh
+++
@@ -1,6 +0,0 @@
-. /users/ntomashenko/work/Challenge/test_run_baseline/venv/bin/activate
-export PATH=/users/ntomashenko/work/Challenge/test_run_baseline/netcdf-c-4.3.3.1/build/bin:$PATH
-export LD_LIBRARY_PATH=/users/ntomashenko/work/Challenge/test_run_baseline/netcdf-c-4.3.3.1/build/bin:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/users/ntomashenko/work/Challenge/test_run_baseline/boost_1_59_0/stage/lib:$LD_LIBRARY_PATH
-export PATH=/users/ntomashenko/work/Challenge/test_run_baseline/nii/CURRENNT_codes/build:$PATH
-export PYTHONPATH=/users/ntomashenko/work/Challenge/test_run_baseline/nii/CURRENNT_codes:/users/ntomashenko/work/Challenge/test_run_baseline/nii/CURRENNT_codes/pyTools:/users/ntomashenko/work/Challenge/test_run_baseline/nii_scripts:$PYTHONPATH
