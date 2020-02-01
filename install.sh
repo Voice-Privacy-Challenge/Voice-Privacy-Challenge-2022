97d77d8b4a012d38fbbd9040f6b7049699ca1866
N.T <tomashenko.n@gmail.com>
2020-02-01 17:38:48 +0100

    add install virtualenv

--- install.sh
+++ install.sh
@@ -22,6 +22,7 @@ currennt_dir=$nii_dir/CURRENNT_codes
 mark=.done-venv
 if [ ! -f $mark ]; then
   echo 'Making python virtual environment'
+  pip3 install virtualenv || exit 1
   python3 -m virtualenv $venv_dir || exit 1
   . $venv_dir/bin/activate
   echo 'Installing python dependencies'
