384fca2dea6585aa3bddc03efb72131e716713ae
N.T <tomashenko.n@gmail.com>
2020-02-03 05:16:37 +0300

    add export to path.sh

--- baseline/path.sh
+++ baseline/path.sh
@@ -1,6 +1,6 @@
 if [ ! -v __PATH_SH__ ]; then
 
-export KALDI_ROOT=`pwd`/../kaldi
+export KALDI_ROOT=$(realpath ../kaldi)
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
@@ -9,10 +9,10 @@ export LC_ALL=C
 . ../env.sh
 
 # based on https://stackoverflow.com/a/5947802/12499892
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m' # No Color
+export GREEN='\033[0;32m'
+export RED='\033[0;31m'
+export NC='\033[0m' # No Color
 
-__PATH_SH__=INCLUDED
+export __PATH_SH__=INCLUDED
 
 fi
