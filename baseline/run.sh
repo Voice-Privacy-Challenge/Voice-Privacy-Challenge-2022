94dcad1f7d5725c6d8221f99f96574021daae9d6
N.T <tomashenko.n@gmail.com>
2020-02-09 15:37:26 +0100

    download_full=false

--- baseline/run.sh
+++ baseline/run.sh
@@ -29,7 +29,7 @@ set -e
 nj=$(nproc)
 stage=0
 
-download_full=true  # If download_full=true all the data that can be used in the training/development will be dowloaded (except for Voxceleb-1,2 corpus); otherwise - only those subsets that are used in the current baseline (with the pretrained models)
+download_full=false  # If download_full=true all the data that can be used in the training/development will be dowloaded (except for Voxceleb-1,2 corpus); otherwise - only those subsets that are used in the current baseline (with the pretrained models)
 data_url_librispeech=www.openslr.org/resources/12  # Link to download LibriSpeech corpus
 data_url_libritts=www.openslr.org/resources/60     # Link to download LibriTTS corpus
 corpora=corpora
