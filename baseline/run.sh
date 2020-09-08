c482e1e4ea74455ea67c74dc0df55b3327c48c39
Andreas Nautsch <andreas.nautsch@eurecom.fr>
2020-09-08 14:29:55 +0200

    run script paths updated

--- baseline/run.sh
+++ baseline/run.sh
@@ -444,9 +444,10 @@ if [ $stage -le 16 ]; then
 fi
 
 
-if [ $stage -le 17]; then
+if [ $stage -le 17 ]; then
   printf "${GREEN}\nStage 17: Summarizing ZEBRA plots for all experiments${NC}\n"
-  PYTHONPATH=$(realpath ../zebra) python ../zebra/demo_voiceprivacy_challenge.py || exit 1
+  mkdir -p voiceprivacy-challenge-2020
+  PYTHONPATH=$(realpath ../zebra) python ../zebra/voiceprivacy_challenge_plots.py || exit 1
 fi
 
 echo Done
