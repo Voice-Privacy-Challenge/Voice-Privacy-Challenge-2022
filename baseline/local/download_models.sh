d019297ea797c3c97b69f5bbf21735f8fef6712c
N.T <tomashenko.n@gmail.com>
2021-12-29 09:14:30 +0100

    download models.2022 and train-clean-360-spk

--- baseline/local/download_models.sh
+++ baseline/local/download_models.sh
@@ -9,16 +9,16 @@ check=$expo/models/asv_eval/xvect_01709_1/final.raw
 if [ ! -f $check ]; then
   mkdir -p $expo
   cd $expo
-  if [ ! -f models.tar.gz ]; then
+  if [ ! -f models.2022.tar.gz ]; then
     echo "  You will be prompted to enter password for getdata@voiceprivacychallenge.univ-avignon.fr"
     sftp getdata@voiceprivacychallenge.univ-avignon.fr <<EOF
 cd challengedata/baseline
-get models.tar.gz
+get models.2022.tar.gz
 bye
 EOF
   fi
   echo '  Unpacking models...'
-  tar -xf models.tar.gz || exit 1
+  tar -xf models.2022.tar.gz || exit 1
   cd $home
 fi
 
