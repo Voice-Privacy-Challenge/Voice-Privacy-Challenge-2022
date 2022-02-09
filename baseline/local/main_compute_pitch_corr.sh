cba9e5470d2c8565a07bbfbbd9a9b3158aa1aceb
N.T <tomashenko.n@gmail.com>
2022-02-09 12:21:09 +0100

    -

--- baseline/local/main_compute_pitch_corr.sh
+++ baseline/local/main_compute_pitch_corr.sh
@@ -8,7 +8,7 @@ set -e
 for suff in $eval_subsets; do
   for data in libri_${suff}_trials_f libri_${suff}_trials_m vctk_${suff}_trials_f vctk_${suff}_trials_m vctk_${suff}_trials_f_common vctk_${suff}_trials_m_common; do
     printf "${BLUE}\n Compute prosody correlation between original and anonymized data for $data${NC}\n"
-    local/compute_pitch_corr.sh --set_test $data || exit 1;
+    local/compute_pitch_corr.sh --data $data || exit 1;
   done
 done
 
