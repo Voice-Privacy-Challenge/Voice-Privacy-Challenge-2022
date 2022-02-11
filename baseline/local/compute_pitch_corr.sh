0d9a877c9226ae73bc7ec7d77f0fe93e9eff9b29
N.T <tomashenko.n@gmail.com>
2022-02-11 04:29:10 +0100

    add f0 correlation metric

--- baseline/local/compute_pitch_corr.sh
+++ baseline/local/compute_pitch_corr.sh
@@ -9,13 +9,18 @@ set -e
 
 #===== begin config =======
 
-data=libri_test_trials_f 
+data=libri_test_trials_f
 
 #=========== end config ===========
 
 . utils/parse_options.sh
 
-echo "data=$data"
+list_name=data/$data/wav.scp 
+list_name_anon=data/$data$anon_data_suffix/wav.scp
 
+python local/pitch_correlation.py \
+      --data=$data --list_name=$list_name --list_name_anon=$list_name_anon --results=$results/results.txt
+
+echo "$list_name, $list_name_anon"
 
 echo '  Done'
--- baseline/run.sh
+++ baseline/run.sh
@@ -150,18 +150,17 @@ fi
 #TODO: add 2 ASR evaluations: orig. and anon.
 
 
-# pitch/prosody correlation
 if [ $stage -le 17 ]; then
-  printf "${GREEN}\nStage 17: Compute pitch correlation metric...${NC}\n"
-  local/main_pitch_corr.sh || exit 1
-fi
-
-
-if [ $stage -le 18 ]; then
-  printf "${GREEN}\nStage 18: Collecting results${NC}\n"
+  printf "${GREEN}\nStage 17: Collecting results${NC}\n"
   local/main_collect_results.sh || exit 1
 fi
 
+
+# pitch/prosody correlation
+if [ $stage -le 18 ]; then
+  printf "${GREEN}\nStage 18: Compute pitch correlation metric...${NC}\n"
+  local/main_compute_pitch_corr.sh || exit 1
+fi
 
 if [ $stage -le 19 ]; then
   printf "${GREEN}\nStage 19: Compute the de-indentification and the voice-distinctiveness preservation with the similarity matrices${NC}\n"
